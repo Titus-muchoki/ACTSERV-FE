848e1f268e09f374a8ffdcd40bb096d3c049fdb2
Eston Kagwima <ekagwima745@gmail.com>
2021-09-24 19:46:39 +0300

    adding url routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 function App() {
   return (
-    <div >
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1 className='text-center'>Welcome to My App</h1>
-
+          <Route path='/' component={HomeScreen} exact />
         </Container>
 
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1 className='text-center'>Welcome to My App</h1>
+
+    </div>
+  )
+}
+
+export default HomeScreen
